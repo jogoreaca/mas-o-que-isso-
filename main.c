57b6767f92908089be538fed8e781626f636c2db
Reaça1 <jogoreaca@gmail.com>
2014-10-16 22:02:19 -0300

    Gitignore renewed

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 
 #include<stdio.h>
 #include "answer.h"
+#define ANSWERTIME 1000 //MileSeconds
 #define EVER ;;
 void EternalLoop(void)
 {
+unsigned int i=0;
 	for(EVER)
 	{
-	printf("mas o que e isso? \n");
+	puts("mas o que e isso?");
+	usleep(ANSWERTIME*1000);
 	}
 }
 int main (int argc, char** argv)
 {
+	unsigned int answerTime=ANSWERTIME*1000;
 	if(argc<2)
 		EternalLoop();
 		
